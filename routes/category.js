f0afd4112ee45fc5a785bca5ea85cfe1add95a18
Sergio Gregorutti <sergiogregorutti@gmail.com>
2021-10-29 10:41:32 -0300

    Add Photo to Categories

--- routes/category.js
+++ routes/category.js
@@ -3,12 +3,13 @@ const router = express.Router();
 
 // Controllers
 const { requireSignin, adminMiddleware } = require('../controllers/auth');
-const { create, categoryById, read, update, remove, list } = require('../controllers/category');
+const { create, categoryById, read, update, remove, list, photo } = require('../controllers/category');
 
 router.get('/category/:categoryId', read);
 router.post('/category/create', requireSignin, adminMiddleware, create);
 router.put('/category/:categoryId', requireSignin, adminMiddleware, update);
 router.delete('/category/:categoryId', requireSignin, adminMiddleware, remove);
+router.get("/category/photo/:categoryId", photo);
 router.get('/categories', list);
 
 router.param('categoryId', categoryById);
